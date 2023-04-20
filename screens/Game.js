8bfa7d1c38d65a4fdb2692f3ec947a22f0c8556f
Tianyi Yang <tianyi_yang@ucsb.edu>
2023-04-19 22:09:34 -0700

    Pressable & text box components of Games.js configured - T. Yang

--- screens/Game.js
+++ screens/Game.js
@@ -1,46 +1,77 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Game({navigation, route}) {
+export default function HomeScreen({navigation}) {
+
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  let textLog = '';
+  if (timesPressed > 1) {
+    textLog = timesPressed + 'x onPress';
+  } else if (timesPressed > 0) {
+    textLog = 'onPress';
+  }
+  let altLog = timesPressed % 2;
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.ScreenTitle}>
-        <Text style={styles.TitleFormat}>Game</Text>
-      </View>
-      <View style={styles.BottonContainer}> 
-      </View>
+	<Pressable
+        onPress={() => {
+          setTimesPressed(current => current + 1);
+        }}
+        style={({pressed}) => [
+          {
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+          },
+          styles.wrapperCustom,
+        ]}>
+        {({pressed}) => (
+          <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me to increment'}</Text>
+        )}
+      </Pressable>
+	<View style={styles.logBox}>
+        <Text testID="pressable_press_console">{textLog}</Text>
+      </View> 
+	<Pressable
+        onPress={() => {
+          setTimesPressed(current => 0); 
+        }}  
+        style={({pressed}) => [
+          {   
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+          },  
+          styles.wrapperCustom,
+        ]}> 
+        {({pressed}) => (
+          <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me to reset'}</Text>
+        )}  
+      </Pressable>
+	<View style={styles.logBox}>
+        <Text testID="pressable_press_console">{altLog}</Text>
+      </View> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
-    padding: 50,
+    justifyContent: 'center',
   },
-  ScreenTitle: {
-    flex: 1,
-    borderWidth: 5,
-    borderColor: "#cccccc",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-    justifyContent: "center",
-    textAlign: "center",
+  text: {
+    fontSize: 32,
+    fontFamily: 'Glametrix',
   },
-  BottonContainer: {
-    flex: 5,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 110,
+  wrapperCustom: {
+    borderRadius: 8,
+    padding: 6,
   },
-  TitleFormat: {
-    fontSize: 30,
-    fontWeight: "bold",
-    fontFamily: "Glametrix",
-    padding: 10,
+  logBox: {
+    padding: 20,
     margin: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#f0f0f0',
+    backgroundColor: '#f9f9f9',
   },
 });
