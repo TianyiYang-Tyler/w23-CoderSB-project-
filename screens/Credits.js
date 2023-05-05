5c35b887e625b9226dfe80eac130468dfa876732
Tianyi Yang <tianyi_yang@ucsb.edu>
2023-05-04 18:24:53 -0700

    T. Yang - Moderates Settings page

--- screens/Credits.js
+++ screens/Credits.js
@@ -8,17 +8,35 @@ const logo = {
 };
 
 const App = () => (
-  <ScrollView>
+  <ScrollView style={styles.paragraphs}>
     <Image source={logo} />
-	<Text fontSize={16}>2022-2023 UCSB CoderSB project series</Text>
-	<Text>Project leader, game & UI designer: <Text style={styles.name}>Tianyi "Tyler" Yang</Text></Text>	
+	<Text style={styles.random}>2022-2023 UCSB CoderSB project series</Text>
+	<Text style={styles.padding}></Text>
+	<Text>Project leader, game & UI designer: <Text style={styles.name}>Tianyi "Tyler" Yang</Text></Text>
+	<Text style={styles.padding}></Text>	
 	<Text>Special thanks to Xinyao Song for setting up initial useNavigators</Text>
     <Image source={logo} />
   </ScrollView>
 );
 
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, () => {
+    return Math.round(Math.random() * 16).toString(16);
+  }); 
+  };
+
 const styles = StyleSheet.create({
-  name: {fontSize: 32, fontFamily: 'Glametrix', color: '#0373fc'}
+  name: {fontSize: 32, fontFamily: 'Glametrix', color: '#0373fc'},
+  paragraphs: {
+    flexDirection: 'column',
+    margin: 40,
+    textAlign: 'center',
+  },
+  padding: {margin: 10,},
+  random: {
+    fontSize: 40,
+    color: randomHexColor(),
+  }
 })
 
 export default App;
